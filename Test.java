1e894157719c455e12606789bae62a342c8c67c9
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2019-08-09 20:31:36 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+public class Test
+{
+Test()
+{
+Frame fr=new Frame();
+Label lb=new Label("mukul");
+fr.add(lb);
+fr.setSize(600,200);
+fr.setVisible(true);
+}
+public static void main(String args[])
+{
+Test ts=new Test()
+}
+}
