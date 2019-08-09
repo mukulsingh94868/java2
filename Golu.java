06e20e23649c8b7a931cda0ac4a8cdc3dfc0386f
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2019-08-09 20:31:10 +0530

    Add files via upload

---
+++ Golu.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+public class Golu
+{
+Golu()
+{
+Frame fr=new Frame();
+Label lb=new Label();
+fr.add(lb);
+fr.setSize(600,300);
+fr.setVisible(true);
+fr.setTitle("mukul");
+fr.setLayout(null);
+}
+public static void main(String agrs[])
+{
+Golu g=new Golu();
+}
+}
