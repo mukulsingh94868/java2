02fd14162042b67c87af4410c0973f329e00e0fa
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-07-28 17:40:04 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,17 @@
+class Demo{
+    public static void main(String args[]){
+     byte a=100;
+     System.out.println(a);
+     short b=5000;
+         System.out.println(b);
+         int c=10000;
+         System.out.println(c);
+         long d=1478779;
+         System.out.println(d);
+
+         boolean isjava = true;
+         boolean isfish = false;
+         System.out.println(isjava);
+         System.out.println(isfish);
+    }
+}
