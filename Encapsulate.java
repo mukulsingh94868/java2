0c49ee0b0e3567d90e8a87b157a4a7c92f7f0996
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-08-05 17:41:43 +0530

    Add files via upload

---
+++ Encapsulate.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Encapsulate {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String abc) {
+		this.name=abc;
+	}
+	public static void main(String[] args) {
+		System.out.println("as fuck");
+	}
+}
